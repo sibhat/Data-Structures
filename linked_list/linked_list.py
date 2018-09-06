ec1b173800964196a402aee467e2bc9df927816d
sibhat <sibhat.temelso@gmail.com>
2018-09-05 18:46:36 -0700

    linked list has passed 4 test cases

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -16,6 +16,12 @@ class Node:
 
   def set_next(self, new_next):
     self.next_node = new_next
+  
+  def __repr__(self):
+    return f"{self.value}"
+
+  def __str__(self):
+    return f"{self.value}"
 
 class LinkedList:
   def __init__(self):
@@ -23,13 +29,57 @@ class LinkedList:
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    new_node = Node(value)
+    if self.head == None:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      curr_node = self.tail
+      curr_node.set_next(new_node)
+      self.tail = new_node
+      
 
   def remove_head(self):
-    pass
+    if self.head == None:
+      return self.head
+    elif self.head is self.tail:
+      node = self.head
+      self.head = node.get_next()
+      self.tail = node.get_next()
+      return node.get_value()
+    else:
+      node = self.head
+      self.head = node.get_next()
+      return node.get_value()
 
   def contains(self, value):
-    pass
+    if self.head == None:
+      return False
+    else:
+      node = self.head
+      while(True):
+        if node.get_value() == value:
+          return True
+        if  node.get_next() == None:
+          break
+        node = node.get_next()
+      
+      return False
 
   def get_max(self):
-    pass
+    max = 0
+    if self.head == None:
+      return None
+    else:
+      node = self.head
+      while(True):
+        if node.get_value() > max:
+          max = node.get_value()
+        if node.get_next() == None:
+          break
+        node = node.get_next()
+
+    return max
+
+
+    
